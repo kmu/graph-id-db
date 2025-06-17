ca6598d222081ba8432a744a81a7bf25ce90cf99
Koki Muraoka <muraok_k@chemsys.t.u-tokyo.ac.jp>
2025-06-17 14:47:30 +0900

    add test files and finder

--- graphid_db/finder.py
+++ graphid_db/finder.py
@@ -1,22 +1,25 @@
 import json
-from typing import Optional, Dict, Any
+from pathlib import Path
+from typing import Any, Dict, Optional
 
 from pymatgen.core import Composition
-from pathlib import Path
 
 DB_PATH = Path(__file__).parent.parent / "raw/id_jsons"
 
 
 class Finder:
-
     def find(self, graph_id: str) -> Optional[Dict[str, Any]]:
         if "-" in graph_id:
             composition_str, graph_hash = graph_id.split("-")
             composition = Composition(composition_str)
 
-            db_path = DB_PATH / composition.reduced_formula / f"{composition.formula}.json"
+            db_path = (
+                DB_PATH
+                / composition.reduced_formula
+                / f"{composition.formula}.json"
+            )
             if db_path.exists():
-                with open(db_path, "r") as f:
+                with open(db_path) as f:
                     docs = json.load(f)
                     return docs.get(graph_hash)
 
