ad7919bf3261ef53ce25d61d451b14acfb41c0af
kamabata <horizon0711pc@gmail.com>
2025-06-18 17:36:44 +0900

    Change finder

--- graphid_db/finder.py
+++ graphid_db/finder.py
@@ -9,29 +9,13 @@ DB_PATH = Path(__file__).parent.parent / "raw/id_jsons"
 
 class Finder:
     def find(self, graph_id: str) -> Optional[Dict[str, Any]]:
-        if "-" in graph_id:
-            composition_str, graph_hash = graph_id.split("-")
-            composition = Composition(composition_str)
+        dir_name = graph_id[:2]
+        file_name = graph_id[:4]
 
-            db_path = (
-                DB_PATH
-                / composition.reduced_formula
-                / f"{composition.formula}.json"
-            )
-            if db_path.exists():
-                with open(db_path) as f:
-                    docs = json.load(f)
-                    return docs.get(graph_hash)
+        db_path = DB_PATH / dir_name / f"{file_name}.json"
+        if db_path.exists():
+            with open(db_path) as f:
+                docs = json.load(f)
+                return docs.get(graph_id)
 
-            return None
-        else:
-            hash_prefix_2 = graph_id[:2]
-            hash_prefix_4 = graph_id[:4]
-
-            db_path = DB_PATH / hash_prefix_2 / f"{hash_prefix_4}.json"
-            if db_path.exists():
-                with open(db_path) as f:
-                    docs = json.load(f)
-                    return docs.get(graph_id)
-
-            return None
+        return None
