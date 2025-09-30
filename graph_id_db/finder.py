96cf5878786159dbd12e09d10053524824e16e15
kamabata <horizon0711pc@gmail.com>
2025-09-30 14:07:33 +0900

    Add AFLOW json files

--- graph_id_db/finder.py
+++ graph_id_db/finder.py
@@ -1,14 +1,39 @@
 from pathlib import Path
 from typing import Optional
+from huggingface_hub import hf_hub_download
 
 import orjson
 
 DB_PATH = Path(__file__).parent.parent / "raw/id_jsons"
 
-
 class Finder:
-    def find(self, graph_id: str) -> Optional[dict[str, list[dict[str, str]]]]:
-        ret_dict: Optional[dict[str, list[dict[str, str]]]] = None
+    def find(self, graph_id: str, is_fast: bool = False) -> list[dict[str, str]]:
+        """
+        Args:
+            graph_id(str): GraphID calculated using graph-id-core 
+            is_fast(bool): If True, find only on-memory entries (from IZA and Materials Project) 
+        """
+        if is_fast:
+            return self.find_fast(graph_id)
+        
+        else:
+            ret_dict = []
+            fast_graph_ids = self.find_fast(graph_id)
+            if fast_graph_ids:
+                ret_dict += fast_graph_ids
+            
+            aflow_graph_ids = self.find_aflow_entries(graph_id)
+            if aflow_graph_ids:
+                ret_dict += aflow_graph_ids
+
+            return ret_dict
+        
+
+    def find_fast(self, graph_id: str) -> list[dict[str, str]]:
+        """
+        Find only on-memory entries.
+        """
+        ret_dict = []
 
         dir_name = graph_id[:2]
         file_name = graph_id[:4]
@@ -18,5 +43,22 @@ class Finder:
             with open(db_path) as f:
                 docs = orjson.loads(f.read())
                 ret_dict = docs.get(graph_id)
+
+        return ret_dict
+
+    def find_aflow_entries(self, graph_id:str) -> list[dict[str, str]]:
+        """
+        Find only AFLOW entries.
+        """
+        ret_dict = []
+        
+        dir_name = graph_id[:2]
+        file_name = graph_id[:4]
+
+        local_path = hf_hub_download(repo_id="kamabata/aflow_graph_ids", filename=f"id_jsons/{dir_name}/{file_name}.json", repo_type="dataset")
+
+        with open(local_path) as f:
+            docs = orjson.loads(f.read())
+            ret_dict = docs.get(graph_id)
 
         return ret_dict
